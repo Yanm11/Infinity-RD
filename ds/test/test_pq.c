55f2ac90cba1dbfb3179bf485d0bed2f6ee724a9
yan.meiri <yanm1@icloud.com>
2024-05-29 23:40:02 +0300

    pq tested

--- ds/test/test_pq.c
+++ ds/test/test_pq.c
@@ -3,15 +3,18 @@
 #include <string.h>
 #include "pq.h"
 
-int compare_func(const void *a, const void *b) {
+int compare_func(const void *a, const void *b) 
+{
     return *(int *)a - *(int *)b;
 }
 
-int match_func(const void *a, void *b) {
+int match_func(const void *a, void *b) 
+{
     return *(int *)a == *(int *)b;
 }
 
-void run_tests() {
+void run_tests() 
+{
     pq_t *pq;
     int data1 = 5;
     int data2 = 10;
@@ -24,64 +27,76 @@ void run_tests() {
     void *erased_data;
 
     pq = PQCreate(compare_func);
-    if (pq == NULL) {
+    if (pq == NULL) 
+    {
         printf("Test 1 failed\n");
         return;
     }
 
     result = PQEnqueue(pq, &data1);
-    if (result != 0) {
+    if (result != 0) 
+    {
         printf("Test 2 failed\n");
     }
 
     result = PQEnqueue(pq, &data2);
-    if (result != 0) {
+    if (result != 0) 
+    {
         printf("Test 3 failed\n");
     }
 
     result = PQEnqueue(pq, &data3);
-    if (result != 0) {
+    if (result != 0) 
+    {
         printf("Test 4 failed\n");
     }
 
     dequeued_data = PQDequeue(pq);
-    if (*(int *)dequeued_data != data3) {
+    if (*(int *)dequeued_data != data3) 
+    {
         printf("Test 5 failed\n");
     }
 
     peeked_data = PQPeek(pq);
-    if (*(int *)peeked_data != data1) {
+    if (*(int *)peeked_data != data1) 
+    {
         printf("Test 6 failed\n");
     }
 
     is_empty = PQIsEmpty(pq);
-    if (is_empty != 0) {
+    if (is_empty != 0) 
+    {
         printf("Test 7 failed\n");
     }
 
     size = PQSize(pq);
-    if (size != 2) {
+    if (size != 2) 
+    {
         printf("Test 8 failed\n");
     }
 
     PQClear(pq);
     size = PQSize(pq);
-    if (size != 0) {
+    if (size != 0) 
+    {
         printf("Test 9 failed\n");
     }
 
     result = PQEnqueue(pq, &data1);
-    if (result != 0) {
+    if (result != 0) 
+    {
         printf("Test 10 failed\n");
     }
 
     erased_data = PQErase(pq, match_func, &data1);
-    if (*(int *)erased_data != data1) {
+    if (*(int *)erased_data != data1) 
+    {
         printf("Test 11 failed\n");
     }
 
     is_empty = PQIsEmpty(pq);
-    if (is_empty != 1) {
+    if (is_empty != 1) 
+    {
         printf("Test 12 failed\n");
     }
 
@@ -90,7 +105,8 @@ void run_tests() {
     printf("All tests passed\n");
 }
 
-int main() {
+int main() 
+{
     run_tests();
     return 0;
 }
